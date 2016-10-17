bce1114cb2c3757fa669b8fb91648dc5205b21c2
Calvin Kniffin <calkniffin@gmail.com>
2016-10-17 14:16:53 -0500

    Set up mongo database and updated package.json (#10)
    * Basic database setup with schema and connections
    
    * Updated package.json for mongo instead of postgesql

---
+++ app/models/graph-schema.js
@@ -0,0 +1,20 @@
+var mongoose = require('mongoose');
+
+//creates a schema for all data to be put in the database
+var graphSchema = mongoose.Schema({
+  //location will be a state -- optional
+  location: String,
+  //income will need to be part of every query
+  income: { type:Number, required: true },
+  //gender optional
+  gender: String,
+  //occupation optional
+  occupation: String,
+  //race optional
+  race: String
+});
+
+var Graph = mongoose.model('Graph', graphSchema);
+
+
+module.exports = Graph;
